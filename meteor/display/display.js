9d5192dbddd97c154217a545363ca2f4e941c3d7
Mathias Decker <mathias@itgweb.de>
2015-01-23 22:55:16 +0100

    some cleanup und color tracking stuff

--- meteor/display/display.js
+++ meteor/display/display.js
@@ -1,25 +1,12 @@
 Courts = new Meteor.Collection("courts");
 Connections = new Meteor.Collection("connections");
-
+Control = new Meteor.Collection("control");
+var colorCounter = 0;
+var colors = ["blue","red","lime","purple","orange","green","yellow","magenta","lime","darkcyan"];
 
 if (Meteor.isClient) {
-
     var thisCourt;
     var thisOldCourt;
-    //Meteor.call("getCourt",function(error,result) {
-    //    if(error){
-    //        //console.log(error.reason);
-    //    }
-    //    else{
-    //        //Template.singleTable.court(result);
-    //        thisCourt = result;
-    //    }
-    //});
-
-
-    //Template.singleTable.rendered = function() {
-    //    console.log("rendered");
-    //}
 
     Template.singleTable.connection = function() {
 
@@ -40,42 +27,46 @@ if (Meteor.isClient) {
                 thisOldCourt = result;
             }
         });
-        //var i = 0;
-        //while ( thisCourt == thisCourtNow && i < 2000) {
-        //    console.log("waiting ..." + thisCourt);
-        //    i = i + 1;
-        //}
-        //console.log("court changed from " + thisCourt + " to " + thisCourtNow);
+
         var r = "";
         Connections.find().forEach(function(row) {
              r = r + " " + row.time;
         });
 
-        //console.log(r);
-        //
-        //if (Connections.findOne({_id: "device-192.168.1.49-Meteor-Server"})) {
-        //    return Connections.findOne({_id: "device-192.168.1.49-Meteor-Server"}).time;
-        //} else {
-        //    return 0;
-        //}
         return r;
     }
 
+    Template.singleTable.control = function() {
+        if(Control.findOne({_id: 'BackgroundTime'}).value != 0) {
+            Meteor.call("getDeviceId",function(error,result) {
+                    if (error) {
+                        //console.log(error.reason);
+                    }
+                    else {
+
+                        $('body').css('backgroundColor', Connections.findOne({_id: result}).color);
+                    }
+                }
+            );
+
+        } else {
+            $('body').css('backgroundColor', 'black');
+        }
+        console.log("colorchange");
+        return Control.findOne({_id: 'BackgroundTime'}).value;
+    }
 
     Template.singleTable.court = function() {
         Meteor.call("registerMonitor");
 
 
         var maxpoints = 21;
-        //if (thisCourt == undefined) {
-            //thisCourt = 1;
-        //}
+
         var c = Courts.findOne({_id: 'court'+thisCourt});
 
         var currentSet = 0;
 
         if(c != undefined) {
-            //console.log("running template");
             // set gray borders -> no bumping around
             c.framep1s1 = 'grayframe'; c.framep1s2 = 'grayframe'; c.framep1s3 = 'grayframe';
             c.framep2s1 = 'grayframe'; c.framep2s2 = 'grayframe'; c.framep2s3 = 'grayframe';
@@ -138,31 +129,69 @@ if (Meteor.isServer) {
     collectionApi = new CollectionAPI({ authToken: '97f0ad9e24ca5e0408a269748d7fe0a0' });
     collectionApi.addCollection(Courts, 'courts');
     collectionApi.addCollection(Connections, 'connections');
+    collectionApi.addCollection(Control, 'control');
     collectionApi.start();
+    //Connections.remove({});
+    Control.remove({});
+    Control.insert({value:"0",_id:"BackgroundTime"});
+
+
+    for(var i = 0; i < Connections.find().count(); i++)
+    {
+        if(Connections.find().fetch()[i]._id.substr(0,7)=="device-") {
+            Connections.update({_id:Connections.find().fetch()[i]._id},{ $set: {color: ''}});
+        }
+    }
   });
 
     Meteor.methods({registerMonitor: function() {
+
         var clientAddress = this.connection.clientAddress;
-        if (clientAddress == '127.0.0.1') {
-            clientAddress = '-Server';
-        } else {
-            clientAddress = '-' + clientAddress.split('.')[3];
+        clientAddress = '-' + clientAddress.split('.')[3];
+
+        // every connection needs to have a color for better visualisation
+        if(Connections.findOne({_id: "device-Meteor" + clientAddress}).color == "") {
+            console.log('give connection a color ' + clientAddress + ' -> ' + colors[colorCounter]);
+            Connections.update({_id: "device-Meteor" + clientAddress}, {$set: {color: colors[colorCounter]}});
+            colorCounter++;
         }
-        serverAddress = this.connection.httpHeaders["x-forwarded-for"]
+        var serverAddress = this.connection.httpHeaders["host"].replace(":3000","");
         return Meteor.http.call("GET","http://"+serverAddress+"/badminton-livescore-v2/output.php?debugid=Meteor" + clientAddress);
-        //return Meteor.http.call("GET","http://192.168.2.104/badminton-livescore-v2/output.php?debugid=Meteor" + clientAddress);
+        
     } });
 
     Meteor.methods({getCourt: function() {
         var clientAddress = this.connection.clientAddress;
-        if (clientAddress == '127.0.0.1') {
-            clientAddress = '-Server';
-        } else {
-            clientAddress = '-' + clientAddress.split('.')[3];
+        //console.log(clientAddress);
+        //if (clientAddress == 'titan') {
+        //    clientAddress = '-Server';
+        //} else {
+        clientAddress = '-' + clientAddress.split('.')[3];
+        //}
+        //var serverAddress = this.connection.httpHeaders["host"].replace(":3000","");
+        //console.log(this.connection.httpHeaders["host"].replace(":3000",""));
+        //console.log(this.connection.httpHeaders["x-forwarded-for"]);
+        //var serverAddress = this.connection.httpHeaders["x-forwarded-for"];
+        try {
+            return Connections.findOne({_id: "device-Meteor" + clientAddress}).court;
         }
-        serverAddress = this.connection.httpHeaders["x-forwarded-for"]
-        return Connections.findOne({_id: "device-"+serverAddress+"-Meteor"+clientAddress}).court;
+        catch(e) {
+            //Connections.update({_id:Connections.findOne({_id: "device-Meteor" + clientAddress})},{ $set: {court: 0}});
+            Connections.insert({_id: "device-Meteor" + clientAddress, "court":"0"})
+            console.log("new connection found -> 0");
+            return 0;
+        }
         //return Connections.findOne({_id: "device-192.168.2.104-Meteor"+clientAddress}).court;
+    } });
+
+    Meteor.methods({getDeviceId: function() {
+        var clientAddress = this.connection.clientAddress;
+        clientAddress = '-' + clientAddress.split('.')[3];
+
+        //var serverAddress = this.connection.httpHeaders["host"].replace(":3000","");
+        //var serverAddress = this.connection.httpHeaders["x-forwarded-for"];
+        //console.log("devid:" + "device-Meteor" + clientAddress);
+        return "device-Meteor" + clientAddress;
     } });
 
 }
