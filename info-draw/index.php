c6d5da211c2b72daaf83c04fd756535eb862fa87
Mathias Decker <mathias@itgweb.de>
2015-03-01 21:56:39 +0100

    added courts in draw overview

--- info-draw/index.php
+++ info-draw/index.php
@@ -4,9 +4,9 @@
 
 <frameset cols="60%,40%" frameborder="no">
 	<frame name="" src="draws.php" />
-  <frameset rows="70%,30%">
+  <frameset rows="50%,50%">
     <frame name="" src="matches.php" />
-    <frame name="" src="../info-game-number" />
+    <frame name="" src="http://localhost:3000" />
   </frameset>
 </frameset>
 
