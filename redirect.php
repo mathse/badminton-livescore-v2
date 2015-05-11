1932e0ca2f1ccc1584e1d7e5443b29cc79f1f395
livescore <github@mathiasdecker.de>
2015-05-11 23:07:50 +0200

    ger rid of meteor

--- redirect.php
+++ redirect.php
@@ -5,5 +5,5 @@ error_reporting(-1);
 ini_set('display_errors', 'Off');
 
 ?>
-<meta http-equiv="refresh" content="0; url=http://<?php echo $_SERVER[SERVER_ADDR]; ?>:3000">
+<meta http-equiv="refresh" content="0; url=http://<?php echo $_SERVER[SERVER_ADDR]; ?>/badminton-livescore-v2/?type=output">
 </html>
