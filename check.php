665e7d576c319babb22866020ca8f1bd7754d654
livescore <github@mathiasdecker.de>
2015-05-11 22:58:21 +0200

    handle missing files better

--- check.php
+++ check.php
@@ -1,14 +1,14 @@
 <html>
 <meta http-equiv="refresh" content="5; URL=check.php?c=<?php echo $_GET['c']; ?>">
 <?php
-$time = (time() - filemtime('sessions/courts/'.$_GET['c']));
+$time = (time() - @filemtime('sessions/courts/'.$_GET['c']));
 if($time < 45) {
 	$color = "black";	
 } else {
 	$color = "red";		
 }
 
-$f = file('sessions/courts/'.$_GET['c']);
+$f = @file('sessions/courts/'.$_GET['c']);
 if($f[7]==0) { 
 	$color = "black"; 
 }
