1e0992b21a7c4fc4e2004aac913c48dd192fca1f
Mathias Decker <mathias@itgweb.de>
2016-08-14 22:18:43 +0200

    read data as json

--- output.php
+++ output.php
@@ -11,17 +11,23 @@ if(!$_GET['input'])
 
 $player1 = '-';
 $player2 = '-';
-$set1p1 = '-';
-$set1p2 = '-';
-$set2p1 = '-';
-$set2p2 = '-';
-$set3p1 = '-';
-$set3p2 = '-';
+for($set=1;$set<=$maxSets;$set++) {
+	$sets[$set]['p1'] = '-';
+	$sets[$set]['p2'] = '-';
+	$sets[$set]['winner'] = 0;
+}
+//print_r($sets);
+//$set1p1 = '-';
+//$set1p2 = '-';
+//$set2p1 = '-';
+//$set2p2 = '-';
+//$set3p1 = '-';
+//$set3p2 = '-';
 $service = '-';
 $currentSet = 1;
-$winnerSet1 = 0;
-$winnerSet2 = 0;
-$winnerSet3 = 0;
+//$winnerSet1 = 0;
+//$winnerSet2 = 0;
+//$winnerSet3 = 0;
 
 if($_GET['debugid']) {
 	$monitorconnection = @file_get_contents('sessions/connections/'.$deviceid);
@@ -43,9 +49,9 @@ if($_GET['direct'])
 if($monitorconnection != '' && file_exists('sessions/courts/'.$monitorconnection))
 {
 	$court = file('sessions/courts/'.$monitorconnection);
-	 
-	$parts_player1 = explode("-",$court[0]);
-	$parts_player2 = explode("-",$court[1]);
+//	print_r(json_decode($court[0]));
+	$parts_player1 = explode("-",json_decode($court[0])->p1);
+	$parts_player2 = explode("-",json_decode($court[0])->p2);
 	$plist = file('players/'.$parts_player1[0].'.txt');
 	
 	
@@ -55,44 +61,52 @@ if($monitorconnection != '' && file_exists('sessions/courts/'.$monitorconnection
 	$player2 = str_replace("&","&amp;",substr(trim($plist[trim($parts_player2[1])]),$seperator+1));
 	$flag1 = substr(trim($plist[trim($parts_player1[1])]),0,$seperator);
 	$flag2 = substr(trim($plist[trim($parts_player2[1])]),0,$seperator);
-	
-	$set1p1 = trim($court[2]);
-	$set1p2 = trim($court[3]);
-	if(trim($court[4])) $set2p1 = trim($court[4]);
-	if(trim($court[5])) $set2p2 = trim($court[5]);
-	if(trim($court[6])) $set3p1 = trim($court[6]);
-	if(trim($court[7])) $set3p2 = trim($court[7]);
-	$service = trim($court[8]);
+
+	for($set=1;$set<=$maxSets;$set++) {
+		$sets[$set]['p1'] = json_decode($court[0])->$set->p1;
+		$sets[$set]['p2'] = json_decode($court[0])->$set->p2;
+	}
+//	$set1p1 = trim($court[2]);
+//	$set1p2 = trim($court[3]);
+//	if(trim($court[4])) $set2p1 = trim($court[4]);
+//	if(trim($court[5])) $set2p2 = trim($court[5]);
+//	if(trim($court[6])) $set3p1 = trim($court[6]);
+//	if(trim($court[7])) $set3p2 = trim($court[7]);
+	$service = json_decode($court[0])->service;
 #	print_r($court);
 
 
 	// set red marker
-	for($i=0;$i<9;$i++)
-	{	
-		if($set1p1==($maxpoints+$i) && $set1p2<($maxpoints-1+$i)) $winnerSet1 = 1;
-		if($set1p2==($maxpoints+$i) && $set1p1<($maxpoints-1+$i)) $winnerSet1 = 2;
+	for($set=1;$set<=$maxSets;$set++) {
+		for ($i = 0; $i < 9; $i++) {
+			if ($sets[$set]['p1'] == ($maxPoints + $i) && $set1p2 < ($maxPoints - 1 + $i)) $sets[$set]['winner'] = 1;
+			if ($sets[$set]['p2'] == ($maxPoints + $i) && $set1p1 < ($maxPoints - 1 + $i)) $sets[$set]['winner'] = 2;
+		}
 	}
-	for($i=0;$i<9;$i++)
-	{	
-		if($set2p1==($maxpoints+$i) && $set2p2<($maxpoints-1+$i)) $winnerSet2 = 1;
-		if($set2p2==($maxpoints+$i) && $set2p1<($maxpoints-1+$i)) $winnerSet2 = 2;
+//	for($i=0;$i<9;$i++)
+//	{
+//		if($set2p1==($maxpoints+$i) && $set2p2<($maxpoints-1+$i)) $winnerSet2 = 1;
+//		if($set2p2==($maxpoints+$i) && $set2p1<($maxpoints-1+$i)) $winnerSet2 = 2;
+//	}
+//	for($i=0;$i<9;$i++)
+//	{
+//		if($set3p1==($maxpoints+$i) && $set3p2<($maxpoints-1+$i)) $winnerSet3 = 1;
+//		if($set3p2==($maxpoints+$i) && $set3p1<($maxpoints-1+$i)) $winnerSet3 = 2;
+//	}
+	for($set=1;$set<=$maxSets;$set++) {
+		if ($sets[$set]['p1'] == $maxPointsStop) $sets[$set]['winner'] = 1;
+		if ($sets[$set]['p2'] == $maxPointsStop) $sets[$set]['winner'] = 2;
 	}
-	for($i=0;$i<9;$i++)
-	{	
-		if($set3p1==($maxpoints+$i) && $set3p2<($maxpoints-1+$i)) $winnerSet3 = 1;
-		if($set3p2==($maxpoints+$i) && $set3p1<($maxpoints-1+$i)) $winnerSet3 = 2;
-	}
-	if($set1p1==30) $winnerSet1 = 1;	
-	if($set1p2==30) $winnerSet1 = 2;
-	if($set2p1==30) $winnerSet2 = 1;	
-	if($set2p2==30) $winnerSet2 = 2;
-	if($set3p1==30) $winnerSet3 = 1;	
-	if($set3p2==30) $winnerSet3 = 2;
-		if($set2p1=='-' && $winnerSet1>0) { $set2p1='0'; }
-		if($set2p2=='-' && $winnerSet1>0) { $set2p2='0'; }
-		if($set3p1=='-' && $winnerSet2>0 && $winnerSet1!=$winnerSet2) { $set3p1='0'; }
-		if($set3p2=='-' && $winnerSet2>0 && $winnerSet1!=$winnerSet2) { $set3p2='0'; }	
-	
+//	if($set2p1==$maxPointsStop) $winnerSet2 = 1;
+//	if($set2p2==$maxPointsStop) $winnerSet2 = 2;
+//	if($set3p1==$maxPointsStop) $winnerSet3 = 1;
+//	if($set3p2==$maxPointsStop) $winnerSet3 = 2;
+
+//	if($set2p1=='-' && $winnerSet1>0) { $set2p1='0'; }
+//	if($set2p2=='-' && $winnerSet1>0) { $set2p2='0'; }
+//	if($set3p1=='-' && $winnerSet2>0 && $winnerSet1!=$winnerSet2) { $set3p1='0'; }
+//	if($set3p2=='-' && $winnerSet2>0 && $winnerSet1!=$winnerSet2) { $set3p2='0'; }
+
 	// set green marker on service
 	if($set1p1>0 || $set1p2>0) $currentSet = 1;
 	if($set2p1>0 || $set2p2>0) $currentSet = 2;
@@ -126,16 +140,14 @@ if($monitorconnection != '' && file_exists('sessions/courts/'.$monitorconnection
 		<flag2><?php echo $flag2; ?></flag2>
 	</names>
 	<sets>
-		<set1p1><?php echo $set1p1; ?></set1p1>
-		<set1p2><?php echo $set1p2; ?></set1p2>
-		<set2p1><?php echo $set2p1; ?></set2p1>
-		<set2p2><?php echo $set2p2; ?></set2p2>
-		<set3p1><?php echo $set3p1; ?></set3p1>
-		<set3p2><?php echo $set3p2; ?></set3p2>
+		<?php	for($set=1;$set<=$maxSets;$set++) {	?>
+		<set<?= $set; ?>p1><?= $sets[$set]['p1']; ?></set<?= $set; ?>p1>
+		<set<?= $set; ?>p2><?= $sets[$set]['p2']; ?></set<?= $set; ?>p2>
+		<?php } ?>
 	</sets>
 	<currentSet><?php echo $currentSet; ?></currentSet>
 	<service><?php echo $service; ?></service>
-	<winnerSet1><?php echo $winnerSet1; ?></winnerSet1>
-	<winnerSet2><?php echo $winnerSet2; ?></winnerSet2>
-	<winnerSet3><?php echo $winnerSet3; ?></winnerSet3>
+	<?php	for($set=1;$set<=$maxSets;$set++) {	?>
+	<winnerSet<?= $set; ?>><?= $sets[$set]['winner'] ?></winnerSet<?= $set; ?>>
+	<?php } ?>
 </r>
