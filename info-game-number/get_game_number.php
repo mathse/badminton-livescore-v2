71e1652fffdeb0130b3d50558f5eee3f81ee2542
livescore <github@mathiasdecker.de>
2016-08-14 14:13:28 +0200

    added progress 'bar'

--- info-game-number/get_game_number.php
+++ info-game-number/get_game_number.php
@@ -1,1 +1,9 @@
-<div style="font-size: <?php echo file_get_contents("../sessions/zoom_gamenumber"); ?>">n&auml;chstes Spiel:<br>#<?php echo count(scandir('../sessions/matchcards/'))-1; ?>
+<?php 
+$max = 126;
+$zoom = file_get_contents("../sessions/zoom_gamenumber");
+$current = count(scandir('../sessions/matchcards/'))-2;
+if($current > $max ) { $current = $max; }
+?>	
+<div style="font-size: <?php echo $zoom; ?>">aktuelles Spiel
+<br>#<?php echo $current; ?> von <?php echo $max; ?> <br><br>
+<table style="height: 200px; width: <?php echo ($current/$max)*100; ?>%"><td style="width:<?php echo ($current/$max)*100; ?>%; background: lime; text-align: right">&nbsp;</td></table>
