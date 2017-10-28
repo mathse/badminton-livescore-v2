447f941407f9da10f2f3600a8b973d8de719cea0
livescore <github@mathiasdecker.de>
2017-10-28 11:38:42 +0200

    changes for 5 matches

--- overview.php
+++ overview.php
@@ -17,18 +17,17 @@ $a = array(
  "TV Emsdetten", //gegner
 );
 */
-include('./players/overview.php');
-/*$a = array(
-array("","","","","",""),
-array("","","","","",""),
-array("","","","","",""),
-array("","","","","",""),
-array("","","","","",""),
-array("","","","","",""),
-array("","","","","",""),
-array("","","","","",""),
-"BW Wittorf", //gegner
-);*/
+//include('./players/overview.php');
+$a = array(
+array("3:2","h","Franke / Dettmann","Laibacher / Haardt"),
+array("1:3","g","Zimmermann / Buchert","Striewski / Darragh"),
+array("0:3","g","Zimmermann / Lehmann","Bald / Westermeyer"),
+array("1:3","g","Lisa Zimmermann","Rachael Darragh"),
+array("3:1","h","Dettmann / Buchert","Haardt / Striewski"),
+array("3:1","h","Robert Franke","Petri Hautala"),
+array("0:3","g","Jan Borsutzki","Malte Laibacher"),
+"BC Hohenlimburg", //gegner
+);
 ?>
 
 <meta http-equiv="refresh" content="5; url=overview.php">
@@ -43,73 +42,97 @@ array("","","","","",""),
     <td>-</td>
     <td colspan="4" style="font-size:2em" nowrap><?php echo $a[count($a)-1]; ?></td>
 </tr>
-<tr>
-    <td style="font-size: 0.9em">1. HD</td>
-    <td style="background: <?php if($a[0][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[0][2]; ?></td>
-    <td/>
-    <td style="background: <?php if($a[0][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[0][3]; ?></td>
-    <td><?php echo $a[0][0]; ?></td>
-</tr>
-<tr>
-    <td style="font-size: 0.9em">DD</td>
-    <td style="background: <?php if($a[1][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[1][2]; ?></td>
-    <td />
-    <td style="background: <?php if($a[1][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[1][3]; ?></td>
-    <td><?php echo $a[1][0]; ?></td>
-</tr>
-<tr>
-    <td style="font-size: 0.9em">2. HD</td>
-    <td style="background: <?php if($a[2][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[2][2]; ?></td>
-    <td />
-    <td style="background: <?php if($a[2][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[2][3]; ?> </td>
-    <td><?php echo $a[2][0]; ?></td>
-</tr>
-<tr>
-    <td style="font-size: 0.9em">DE</td>
-    <td style="background: <?php if($a[3][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[3][2]; ?> </td>
-    <td />
-    <td style="background: <?php if($a[3][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[3][3]; ?> </td>
-    <td><?php echo $a[3][0]; ?></td>
-</tr>
-<tr>
-    <td style="font-size: 0.9em">Mixed</td>
-    <td style="background: <?php if($a[4][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[4][2]; ?> </td>
-    <td />
-    <td style="background: <?php if($a[4][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[4][3]; ?> </td>
-    <td><?php echo $a[4][0]; ?></td>
-</tr>
-<tr>
-    <td style="font-size: 0.9em">1. HE</td>
-    <td style="background: <?php if($a[5][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[5][2]; ?> </td>
-    <td />
-    <td style="background: <?php if($a[5][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[5][3]; ?></td>
-    <td><?php echo $a[5][0]; ?></td>
-</tr>
-<tr>
-    <td style="font-size: 0.9em">2. HE</td>
-    <td style="background: <?php if($a[6][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[6][2]; ?> </td>
-    <td />
-    <td style="background: <?php if($a[6][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[6][3]; ?> </td>
-    <td><?php echo $a[6][0]; ?></td>
-</tr>
-<tr><td/><td style="font-size:4em" align="right">
-<?
-for($i=0;$i<7;$i++) {
-	if($a[$i][1] == "h") {
-		$h++;
-	}
-}
-echo $h;
+<?php
+if ($a[0][3] != '') {
+   ?>
+  <tr>
+      <td style="font-size: 0.9em">1. HD</td>
+      <td style="background: <?php if($a[0][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[0][2]; ?></td>
+      <td/>
+      <td style="background: <?php if($a[0][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[0][3]; ?></td>
+      <td><?php echo $a[0][0]; ?></td>
+  </tr>
+  <tr>
+      <td style="font-size: 0.9em">DD</td>
+      <td style="background: <?php if($a[1][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[1][2]; ?></td>
+      <td />
+      <td style="background: <?php if($a[1][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[1][3]; ?></td>
+      <td><?php echo $a[1][0]; ?></td>
+  </tr>
+  <tr>
+      <td style="font-size: 0.9em">2. HD</td>
+      <td style="background: <?php if($a[2][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[2][2]; ?></td>
+      <td />
+      <td style="background: <?php if($a[2][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[2][3]; ?> </td>
+      <td><?php echo $a[2][0]; ?></td>
+  </tr>
+  <tr>
+      <td style="font-size: 0.9em">DE</td>
+      <td style="background: <?php if($a[3][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[3][2]; ?> </td>
+      <td />
+      <td style="background: <?php if($a[3][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[3][3]; ?> </td>
+      <td><?php echo $a[3][0]; ?></td>
+  </tr>
+  <tr>
+      <td style="font-size: 0.9em">Mixed</td>
+      <td style="background: <?php if($a[4][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[4][2]; ?> </td>
+      <td />
+      <td style="background: <?php if($a[4][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[4][3]; ?> </td>
+      <td><?php echo $a[4][0]; ?></td>
+  </tr>
+  <tr>
+      <td style="font-size: 0.9em">1. HE</td>
+      <td style="background: <?php if($a[5][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[5][2]; ?> </td>
+      <td />
+      <td style="background: <?php if($a[5][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[5][3]; ?></td>
+      <td><?php echo $a[5][0]; ?></td>
+  </tr>
+  <tr>
+      <td style="font-size: 0.9em">2. HE</td>
+      <td style="background: <?php if($a[6][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[6][2]; ?> </td>
+      <td />
+      <td style="background: <?php if($a[6][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[6][3]; ?> </td>
+      <td><?php echo $a[6][0]; ?></td>
+  </tr>
+<?php
+if(count($a)==9) {
 ?>
-</td><td>:</td><td style="font-size:4em">
-<?
-for($i=0;$i<8;$i++) {
-	if($a[$i][5] == "g") {
-		$g++;
-	}
+  <tr>
+      <td style="font-size: 0.9em">3. HE</td>
+      <td style="background: <?php if($a[7][1] == 'h') { echo 'lime'; } ?>"><?php echo $a[7][2]; ?> </td>
+      <td />
+      <td style="background: <?php if($a[7][1] == 'g') { echo 'lime'; } ?>"><?php echo $a[7][3]; ?> </td>
+      <td><?php echo $a[7][0]; ?></td>
+  </tr>
+
+<?php } ?>
+  <tr><td/><td style="font-size:4em" align="right">
+  <?
+  for($i=0;$i<8;$i++) {
+  	if($a[$i][1] == "h") {
+  		$h++;
+  	}
+  }
+  echo $h;
+  ?>
+  </td><td>:</td><td style="font-size:4em">
+  <?
+  for($i=0;$i<8;$i++) {
+  	if($a[$i][1] == "g") {
+  		$g++;
+  	}
+  }
+  echo $g;
+  ?>
+  </td></tr>
+  <?php
+} else {
+  ?>
+  <tr>
+    <td colspan="5" style="text-align: center"><br>Aufstellung folgt</td>
+  </tr>
+  <?php
 }
-echo $g;
 ?>
-</td></tr>
 </table>
 </center>
